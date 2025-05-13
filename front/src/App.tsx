de4a01454c5957ca5e4c893483d3d6d0448c3c03
johannes60 <garilsenan@gmail.com>
2025-05-13 16:53:10 +0200

    feat: add route for CreateTalk page in App component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,20 @@
 import "./style/global.css";
-import CreateTalk from "./pages/CreateTalk";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./components/auth/Homepage.tsx";
+import Login from "./components/auth/Login.tsx";
+import CreateTalk from "./pages/CreateTalk.tsx";
 
 const App = () => {
   return (
-    <div>
-      <CreateTalk />
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/create-talk" element={<CreateTalk />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
