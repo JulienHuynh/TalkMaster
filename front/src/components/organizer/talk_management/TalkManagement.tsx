94f46cf3f6e3d25c45cba3884a712af017a61413
Samuel Lunion <lunion.sam@gmail.com>
2025-05-13 11:28:55 +0200

    feat : css mobile first

--- front/src/components/organizer/talk_management/TalkManagement.tsx
+++ front/src/components/organizer/talk_management/TalkManagement.tsx
@@ -1,39 +1,41 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-const bull = (
-    <Box
-        component="span"
-        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-        •
-    </Box>
-);
-
 const TalkManagement: React.FC = () => {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Demande de talks</h1>
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-                        Word of the Day
-                    </Typography>
-                    <Typography variant="h5" component="div">
-                        be{bull}nev{bull}o{bull}lent
-                    </Typography>
-                    <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
-                    <Typography variant="body2">
-                        well meaning and kindly.
-                        <br />
-                        {'"a benevolent smile"'}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </>
+        <div className={"border "}>
+            <div className={"flex-col items-center"}>
+                <h1 className="text-3xl font-bold underline text-center mb-5">Demandes de talks</h1>
+                <Card sx={{minWidth: 275, marginBottom: 3}}>
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            TALK 1
+                        </Typography>
+                        <Typography sx={{color: 'text.secondary', mb: 1.5}}>Sujet 1</Typography>
+                        <Typography variant="body2">
+                            12/02/2025
+                            <br/>
+                            14:30 - 15:00
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card sx={{minWidth: 275, marginBottom: 3}}>
+                    <CardContent>
+                        <Typography variant="h5" component="div">
+                            TALK 1
+                        </Typography>
+                        <Typography sx={{color: 'text.secondary', mb: 1.5}}>Sujet 1</Typography>
+                        <Typography variant="body2">
+                            12/02/2025
+                            <br/>
+                            14:30 - 15:00
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
     );
 };
 
