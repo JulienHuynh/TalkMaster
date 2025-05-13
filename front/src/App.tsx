a208b30c0c7f4f05cd5779e1add89b1361b41878
johannes60 <garilsenan@gmail.com>
2025-05-13 16:11:41 +0200

    feat: implement CreateTalk component with form functionality and styling

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,18 +1,11 @@
 import "./style/global.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from "./components/auth/Homepage.tsx";
-import Login from "./components/auth/Login.tsx";
+import CreateTalk from "./pages/CreateTalk";
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
-    </>
+    <div>
+      <CreateTalk />
+    </div>
   );
 };
 
---
+++ front/src/utils/helper.ts
@@ -0,0 +1,18 @@
+
+export const fieldSx = {
+    // Applies to input and textarea
+    "& .MuiInputBase-input": {
+      color: "#FFFFFF",
+      "&::placeholder": { color: "#FFFFFF" },
+    },
+    // Label color
+    "& .MuiInputLabel-root": {
+      color: "#AAAAAA",
+    },
+    // Outline border
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": { borderColor: "#555555" },
+      "&:hover fieldset": { borderColor: "#555555" },
+      "&.Mui-focused fieldset": { borderColor: "#555555" },
+    },
+  };
