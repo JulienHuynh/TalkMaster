a82a571278eae222c4a3667e9e79d13a9b034571
hugolhld <hugo.lhernould@gmail.com>
2025-05-13 14:03:42 +0200

    refactor: talks routes

--- back/src/routes/talk.ts
+++ back/src/routes/talk.ts
@@ -1,35 +1,57 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 
 export default function talksRoutes(prisma: PrismaClient): Router {
   const router = Router();
 
+  router.get('/', async (_, res) => {
+    const talks = await prisma.talk.findMany();
+    res.json(talks);
+  });
+
+  router.get('/:id', async (req: Request<{ id: string; }>, res: any) => {
+    const { id } = req.params;
+    const talk = await prisma.talk.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!talk) {
+      return res.status(404).json({ error: 'Talk not found' });
+    }
+
+    res.json(talk);
+  });
+
   // Talk Creation
-  router.post('/', async (req, res) => {
-    const { title, description, roomId, userId } = req.body;
+  router.post('/', async (req: Request<{ title: string, description: string, status: string, roomId: number, userId: string }>, res: any) => {
+    const { title, description, status = 'pending', roomId, userId } = req.body;
 
     if (!title || !roomId || !userId) {
       return res.status(400).json({ error: 'title, roomId et userId sont obligatoires' });
     }
 
-    try {
-      const talk = await prisma.talk.create({
-        data: {
-          title,
-          description,
-          roomId,
-          userId,
-        },
-      });
+    await prisma.talk.create({
+      data: {
+        title,
+        description,
+        status,
+        roomId,
+        userId,
+      },
+    }).then((talk: object) => {
       res.status(201).json(talk);
-    } catch (err) {
-      console.error(err);
-      res.status(400).json({ error: 'Impossible de créer le talk' });
-    }
+    })
+      .catch((err: object) => {
+        console.error(err);
+        res.status(400).json({ error: 'Impossible de créer le talk' });
+      });
+
   });
 
   //Talk Modification
-  router.put('/:id', async (req, res) => {
+  router.put('/:id', async (req: Request<{ id: number, title: string, description: string, status: string, roomId: number, userId: string }>, res: any) => {
     const { id } = req.params;
     const { title, description, roomId, status, userId } = req.body;
 
@@ -37,36 +59,35 @@ export default function talksRoutes(prisma: PrismaClient): Router {
       return res.status(400).json({ error: 'userId est requis pour modifier un talk' });
     }
 
-    try {
-      const talk = await prisma.talk.findUnique({ where: { id: parseInt(id) } });
+    const talk = await prisma.talk.findUnique({ where: { id: id } })
 
-      if (!talk) {
-        return res.status(404).json({ error: 'Talk introuvable' });
-      }
-
-      if (talk.userId !== userId) {
-        return res.status(403).json({ error: 'Non autorisé à modifier ce talk' });
-      }
-
-      const updatedTalk = await prisma.talk.update({
-        where: { id: parseInt(id) },
-        data: {
-          title,
-          description,
-          roomId,
-          status,
-        },
-      });
-
-      res.json(updatedTalk);
-    } catch (err) {
-      console.error(err);
-      res.status(400).json({ error: 'Impossible de modifier le talk' });
+    if (talk.userId !== userId) {
+      return res.status(403).json({ error: 'Non autorisé à modifier ce talk' });
     }
+
+    if (!talk) {
+      return res.status(404).json({ error: 'Talk introuvable' });
+    }
+
+    await prisma.talk.update({
+      where: { id: id },
+      data: {
+        title,
+        description,
+        roomId,
+        status,
+      },
+    }).then((updatedTalk: object) => {
+      res.status(200).json(updatedTalk);
+    })
+      .catch((err: object) => {
+        console.error(err);
+        res.status(400).json({ error: 'Impossible de modifier le talk' });
+      });
   });
 
   // Delete a Talk
-  router.delete('/:id', async (req, res) => {
+  router.delete('/:id', async (req: Request<{ id: number, userId: string }>, res: any) => {
     const { id } = req.params;
     const { userId } = req.body;
 
@@ -74,24 +95,25 @@ export default function talksRoutes(prisma: PrismaClient): Router {
       return res.status(400).json({ error: 'userId est requis pour supprimer un talk' });
     }
 
-    try {
-      const talk = await prisma.talk.findUnique({ where: { id: parseInt(id) } });
+    const talk = await prisma.talk.findUnique({ where: { id: id } });
 
-      if (!talk) {
-        return res.status(404).json({ error: 'Talk introuvable' });
-      }
-
-      if (talk.userId !== userId) {
-        return res.status(403).json({ error: 'Non autorisé à supprimer ce talk' });
-      }
-
-      await prisma.talk.delete({ where: { id: parseInt(id) } });
-
-      res.status(204).send();
-    } catch (err) {
-      console.error(err);
-      res.status(400).json({ error: 'Impossible de supprimer le talk' });
+    if (talk.userId !== userId) {
+      return res.status(403).json({ error: 'Non autorisé à supprimer ce talk' });
     }
+
+    if (!talk) {
+      return res.status(404).json({ error: 'Talk introuvable' });
+    }
+
+
+    await prisma.talk.delete({ where: { id } })
+      .then(() => {
+        res.status(200).json({ message: 'Talk supprimé avec succès' });
+      })
+      .catch((err: object) => {
+        console.error(err);
+        res.status(400).json({ error: 'Impossible de supprimer le talk' });
+      });
   });
 
   return router;
