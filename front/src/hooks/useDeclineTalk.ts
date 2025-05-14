4f0fe5baee477ead15fb20c0176183209b29c1e8
Mehdi-Mah <mdi.mahmoudi@gmail.com>
2025-05-14 14:02:17 +0200

    refactor: feedback route handle state talk + refactor

--- front/src/hooks/useDeclineTalk.ts
+++
@@ -1,9 +0,0 @@
-export default function useDeclineTalk(talkID: number) {
-    return fetch(`${import.meta.env.VITE_API_HOST}/organizer/talks/${talkID}/refused`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then(data => data)
-}
