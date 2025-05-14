5e2bdc4c95a3d382751bbb3f19798e50a65a23e8
Mehdi-Mah <mdi.mahmoudi@gmail.com>
2025-05-14 14:02:17 +0200

    refactor: feedback route handle state talk + refactor

--- back/src/routes/organizerTalks.ts
+++ back/src/routes/organizerTalks.ts
@@ -1,35 +1,19 @@
-import { Router } from 'express';
+import { Router, Request } from 'express';
 import { PrismaClient } from '@prisma/client';
 
 export default function organizerTalksRoute(prisma: PrismaClient): Router {
 
   const router = Router();
 
-  // Route pour valider un talk
-  router.post('/:id/validated', async (req, res) => {
+  // Route pour update le status d'un talk"
+  router.post('/:id/update', async (req: Request<{ id: string, status: string }>, res) => {
     const { id } = req.params;
+    const { status } = req.body;
 
     await prisma.talk.update({
       where: { id: Number(id)},
       data: {
-        status: 'validated'
-      }
-    })
-    .then((updatedTalk: object) => res.status(200).json(updatedTalk))
-    .catch((err: object) => {
-      console.log(err)
-      res.status(400).json({ error: 'Enable to update talk' });
-    });
-  });
-
-  // Route pour refuser un talk
-  router.post('/:id/refused', async (req, res) => {
-    const { id } = req.params;
-
-    await prisma.talk.update({
-      where: { id: Number(id)},
-      data: {
-        status: 'refused'
+        status: status
       }
     })
     .then((updatedTalk: object) => res.status(200).json(updatedTalk))
--- front/src/hooks/useValidateTalk.ts
+++
@@ -1,9 +0,0 @@
-export default function useValidateTalk(talkID: number) {
-    return fetch(`${import.meta.env.VITE_API_HOST}/organizer/talks/${talkID}/validated`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-        .then(data => data)
-}
