dfa01ca8bc39cd664f89f38e9e74d03bb540a139
Mehdi-Mah <mdi.mahmoudi@gmail.com>
2025-05-14 09:24:53 +0200

    feature: add route for refuse and accepte talk

--- back/src/index.ts
+++ back/src/index.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import { PrismaClient } from '../generated/prisma';
 import usersRoutes from './routes/user';
 import talksRoutes from './routes/talk';
+import organizerTalksRoute from './routes/organizerTalks';
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use('/users', usersRoutes(prisma));
 app.use('/talks', talksRoutes(prisma));
+app.use('/organizer/talks', organizerTalksRoute(prisma));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
